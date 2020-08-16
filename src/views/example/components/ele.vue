ca0f9942aa038f2b47167c573d517afe46552e5b
chenglei <chenglei1@lvmama.com>
2020-08-16 17:01:48 +0800

    分类管理，左树右表

---
+++ src/views/example/components/ele.vue
@@ -0,0 +1,47 @@
+<template>
+<el-tree
+  :data="data"
+  :props="defaultProps"
+
+  @node-click="handleNodeClick">
+</el-tree>
+</template>
+<script>
+import Bus from './bus.js'
+export default {
+  data() {
+    return {
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+        }, {
+          label: '二级 2-2',
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+        }, {
+          label: '二级 3-2',
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data.$treeNodeId);
+      Bus.$emit('val',data.$treeNodeId);
+    }
+  }
+};
+</script>
---
+++ src/views/example/components/etable.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import Bus from './bus.js'
+
+export default {
+  name: "eatable",
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  mounted() {
+    Bus.$on('val', (data) => {
+      console.log('接收到的数据是' + data)
+      if (data === 1) {
+        this.tableData[0].date = "我改变了"
+      }
+    })
+  }
+}
+</script>
